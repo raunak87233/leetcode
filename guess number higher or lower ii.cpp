2833d8b8609760d74e153b9073583d51d5c189f2
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2022-12-21 16:38:28 +0530

    Create guess number higher or lower ii.cpp

---
+++ guess number higher or lower ii.cpp
@@ -0,0 +1,17 @@
+int guessNumber(int n){
+   int left = 1, right = n;
+        while (left <= n) {
+            int mid = left + (right - left) / 2;
+            int g = guess(mid);
+            if (g == 0) {
+                return mid;
+            }
+            else if (g == 1) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return 0;
+    }
