09988611ec175af669f194431dcf7a36db5cf611
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2022-12-29 16:38:06 +0530

    Create Merge Sorted Array.cpp

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>ans;
+        int i=0, j=0;
+        m=nums1.size();
+        n=nums2.size();
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+                
+            }
+                else {
+                    ans.push_back(nums2[j]);
+                    j++;
+                   
+                }
+        }
+                    while(i<m){
+                        ans.push_back(nums1[i]);
+                        i++;
+                        
+                    }
+                        while(j<n){
+                            ans.push_back(nums2[j]);
+                            j++;
+                            
+                        }
+                        nums1=ans;
+                    }
+
+
+};
